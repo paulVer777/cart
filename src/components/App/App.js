cc6d661e4de181313d5225ce4ab7e4c49bf07a4c
Paweł Polit <pawelpolit@MacBook-Pro-Pawe.local>
2021-02-18 23:33:53 +0100

    cart-update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,77 +1,110 @@
-import React, { useEffect, useState } from 'react';
+  import React, { useEffect, useState } from 'react';
 
-import { CartControl } from "./CartControl";
-import { operations } from "../../constants/constants";
+  import CartControl from "./CartControl";
+  import { operations } from "../../constants/constants";
 
-import "./App.css";
+  import "./App.css";
 
-const App = () => {
+  const App = () => {
 
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [isBlocked, setIsBlocked] = useState(false);
+    const [activePid, setActivePid] = useState(false);
+    const [operationType, setOperationType] = useState(null);
 
-  useEffect(() => {
-    fetch('/api/cart').then((res) => res.json()).then(res => {
-      const productsListWithMinimumQuantity = res.map(product => ({ ...product, quantity: product.min }));
-      setProducts(productsListWithMinimumQuantity)
-    });
-  }, []);
+    useEffect(() => {
+      getProductListToCart();
+    }, []);
 
-  const changeProductQuantity = (pid, quantity, operationType, min) => {
-    
-    const dataToSend = {
-      pid,
-      quantity
-    };
+    const getProductListToCart = () => {
+      fetch('/api/cart').then((res) => res.json()).then(res => {
+        const productsListWithMinimumQuantity = res.map(product => ({ ...product, quantity: product.min }));
+        setProducts(productsListWithMinimumQuantity)
+      });
+    }
 
-    fetch('/api/product/check', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dataToSend),
-    }).then((res) => res.json()).then(res => {
-      
-      const isError = res.isError;
+    const shouldBlockButtons = (status) => setIsBlocked(status);
 
-      setProducts(products.map(product => {
-        if (product.pid === pid) {
-          if(isError){
+    const activePidHandler = (pid) => setActivePid(pid);
+
+    const operationTypeHandler = (type) => setOperationType(type);
+
+    const changeProductQuantity = (pid, quantity, operationType, min, max) => {
+
+      const actualProductQuantity = products.find(product => product.pid === pid).quantity;
+
+      const dataToSend = {
+        pid,
+        quantity
+      };
+
+      fetch('/api/product/check', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      }).then((res) => res.json()).then(res => {
+
+        const isError = res.isError;
+        const isQuantityExceeded = actualProductQuantity + quantity > max;
+
+        setProducts(products.map(product => {
+          if (product.pid === pid) {
+            if (isError || isQuantityExceeded) {
+              shouldBlockButtons(false);
+              setOperationType(null);
+
+              return {
+                ...product,
+                quantity: min
+              }
+            };
+
+            shouldBlockButtons(false);
+            setOperationType(null);
+
             return {
               ...product,
-              quantity: min
-            }
+              quantity: operationType === operations.ADD ? product.quantity + quantity : product.quantity - quantity
+            };
           };
+          return product;
+        }));
+      })
+        .catch((error) => console.log(error));
+    };
 
-          return {
-            ...product,
-            quantity: operationType === operations.ADD ? product.quantity + quantity : product.quantity - quantity
-          };
-        };
-        return product;
-      }));
-    })
-    .catch((error) => console.log(error));
+    const totalCalculated = products.map(product => product.quantity * product.price).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+    return (
+      <div className="container">
+        <h3>Lista produktów</h3>
+        {!products.length && <p>Ładuję produkty...</p>}
+        <ul>
+          {
+            products.map((product) => <li className="row" key={product.pid}>
+              <span className="row__product-info">{`${product.name}, cena: ${product.price} zł`}</span>
+              <CartControl
+                quantity={product.quantity}
+                pid={product.pid}
+                changeProductQuantity={changeProductQuantity}
+                products={products} product={product}
+                isBlocked={isBlocked}
+                shouldBlockButtons={shouldBlockButtons}
+                activePidHandler={activePidHandler}
+                activePid={activePid}
+                operationTypeHandler={operationTypeHandler}
+                operationType={operationType}
+              />
+            </li>)
+          }
+        </ul>
+        <p>{`Suma zamówienia: ${totalCalculated.toFixed(2)} zł`}</p>
+      </div>
+    );
   };
 
-  const totalCalculated = products.map( product => product.quantity * product.price).reduce( (previousValue, currentValue) => previousValue + currentValue,0 );
-  
-  return (
-    <div className="container">
-      <h3>Lista produktów</h3>
-      {!products.length && <p>Ładuję produkty...</p>}
-      <ul>
-        {
-          products.map((product) => <li className="row" key={product.pid}>
-            <span className="row__product-info">{`${product.name}, cena: ${product.price} zł`}</span>
-            <CartControl quantity={product.quantity} pid={product.pid} changeProductQuantity={changeProductQuantity} products={products} product={product} isBlocked={false} />
-          </li>)
-        }
-      </ul>
-      <p>{`Suma zamówienia: ${totalCalculated.toFixed(2)} zł`}</p>
-    </div>
-  );
-};
-
-export {
-  App
-};
+  export {
+    App
+  };
--- src/components/App/CartControl.js
+++ src/components/App/CartControl.js
@@ -1,42 +1,65 @@
-import React, { useState, useCallback } from 'react';
+    import React, { useState, useCallback, memo } from 'react';
 
-import { operations } from "../../constants/constants";
-import { debounce } from "../../utils/utils";
+    import { operations } from "../../constants/constants";
+    import { debounce } from "../../utils/utils";
 
-import './App.css';
+    import './App.css';
 
-const CartControl = ({ changeProductQuantity, products, product, isBlocked }) => {
+    const CartControl = ({
+      changeProductQuantity,
+      products,
+      product,
+      isBlocked,
+      shouldBlockButtons,
+      activePid,
+      activePidHandler,
+      operationTypeHandler,
+      operationType
+    }) => {
+      const { quantity, pid, min, max } = product;
 
-  const { quantity, pid, min, max } = product;
+      const [productAmount, setProductAmount] = useState(0);
 
-  const [productAmount, setProductAmount] = useState(0);
+      const onChangeProductAmountDebounced = useCallback(debounce((pid, productQty, operationType, min, max) => {
+        changeProductQuantity(pid, productQty, operationType, min, max);
+        setProductAmount(0);
+      }, 3000), [products])
 
-  const onChangeProductAmountDebounced = useCallback(debounce((pid, productQty, operationType, min) => {
-    changeProductQuantity(pid, productQty, operationType, min);
-    setProductAmount(0);
-  }, 3000), [products])
 
-  return (
-    <div className="cart-control">
-      <button onClick={
-        () => {
-          setProductAmount(prevAmount => prevAmount + 1);
-          onChangeProductAmountDebounced(pid, productAmount + 1, operations.ADD, min)
-        }
-      }
-        disabled={quantity === max || isBlocked}
-      >+</button>
-      <p>{`Obecnie masz ${quantity} sztuk produktu`}</p>
-      <button onClick={() => {
+      const onClickHandler = (operation) => () => {
+        activePidHandler(pid)
+        shouldBlockButtons(true)
         setProductAmount(prevAmount => prevAmount + 1);
-        onChangeProductAmountDebounced(pid, productAmount + 1, operations.SUBSTRACT, min)
+        onChangeProductAmountDebounced(pid, productAmount + 1, operation, min, max)
+        operationTypeHandler(operation)
       }
-      }
-        disabled={quantity === min || isBlocked}>-</button>
-    </div>
-  );
-};
+     
 
-export {
-  CartControl
-};
+      //Disable button conditions
+      const isOperationOnCartInProgressAndPidIsDifferentThanActiveOne = isBlocked && activePid !== pid;
+
+      const increasingProductQuantityInprogress = operationType === operations.ADD;
+      const decreasingProductQuantityInprogress = operationType === operations.SUBSTRACT;
+
+      const isMinQuantityReached = quantity === min;
+      const isMaxQuantityReached = quantity === max;
+
+      return (
+        <div className="cart-control">
+          <button onClick={
+            onClickHandler(operations.ADD)
+          }
+            disabled={isMaxQuantityReached || isOperationOnCartInProgressAndPidIsDifferentThanActiveOne || decreasingProductQuantityInprogress}
+          >+</button>
+          {isBlocked && activePid === pid ? <p>Przetwarzanie...</p> : <p>{`Obecnie masz ${quantity} sztuk produktu`}</p>}
+          <button onClick={
+            onClickHandler(operations.SUBSTRACT)
+          }
+            disabled={isMinQuantityReached || isOperationOnCartInProgressAndPidIsDifferentThanActiveOne || increasingProductQuantityInprogress}
+          >-</button>
+        </div>
+      );
+    };
+
+    export default memo(CartControl);
+    
